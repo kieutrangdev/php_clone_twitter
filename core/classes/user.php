be39f9c9ecaaca2ac4d0525c7a2f5e3da2548228
kieutrangdev <104716412+kieutrangdev@users.noreply.github.com>
2022-06-04 15:57:09 -0700

    create sign-in sign-out

--- core/classes/user.php
+++ core/classes/user.php
@@ -15,6 +15,19 @@
             return $var;
         }
 
+        public function checkEmail($email)
+        {
+            $stmt = $this->pdo->prepare("SELECT `email` FROM `users` WHERE `email` = :email ");
+            $stmt->bindParam(":email", $email, PDO::PARAM_STR);
+            $stmt->execute();
+
+            $count = $stmt->rowCount();
+            if($count >0) {
+                return true;
+            }
+            else return false;
+        }
+
         public function login($email, $password)
         {
             $stmt = $this->pdo->prepare("
@@ -42,10 +55,45 @@
             }
         }
 
+
+        public  function logout()
+        {
+            $_SESSION = array();
+            session_destroy();
+            header("location: ../index.php");
+        }
+
+
         public function userData($id)
         {
-            $stmt = $this->pdo->
+            $stmt = $this->pdo->prepare("SELECT * FROM `users` WHERE `id` = :id ");
+            $stmt->bindParam(":id",$id,PDO::PARAM_INT);
+            $stmt->execute();
+
+            return $stmt->fetch(PDO::FETCH_OBJ);
 
         }
+
+       public function register($email, $screenName, $password)
+       {
+           $stmt = $this -> pdo-> prepare("INSERT INTO `users` (`email`,`password`,`screenName`,`profileImage`,`profileCover`)
+            VALUES (:email,:password, :screenName, 'assets/images/defaultprofileimage.png','assets/images/defaultCoverImage.png')
+            ");
+
+
+           $stmt->bindParam(":email", $email ,PDO::PARAM_STR);
+           $stmt->bindParam(":password", md5($password) ,PDO::PARAM_STR);
+           $stmt->bindParam(":screenName", $screenName ,PDO::PARAM_STR);
+
+            $stmt->execute();
+           $id = $this->pdo->lastInsertId();
+           $_SESSION['id'] = $id;
+
+       }
+
+
+
+
+
     }
 ?>
--- includes/sign-up.php
+++ includes/sign-up.php
@@ -1,4 +1,55 @@
 <?php
+
+
+
+    if(isset($_POST['signup']))
+    {
+        $screenName = $_POST['screenName'];
+        $password = $_POST['password'];
+        $email = $_POST['email'];
+
+        $error = '';
+
+        if(empty($screenName) or empty($password) or empty($email))
+        {
+            $error = "All fields are required";
+        }
+        else {
+            $email = $getFromU->checkInput($email);
+            $password = $getFromU->checkInput($password);
+            $screenName = $getFromU->checkInput($screenName);
+            if(!filter_var($email))
+            {
+                $error = "Invalid email format";
+
+            }
+            else if(strlen($screenName) > 20)
+            {
+                $error = "Name must be betweet in 6-20 characters";
+
+            }
+            else if(strlen($password) < 5)
+            {
+                $error = "Password is too short";
+
+            }
+            else {
+                if($getFromU->checkEmail($email) === true)
+                {
+                    $error = "Email is already in use";
+                }
+                else {
+                    $getFromU->register($email,$screenName,$password);
+                    header("Location: home.php");
+                }
+            }
+        }
+    }
+
+    else {
+        $error = null;
+    }
+
 ?>
 
 <form method="post">
@@ -6,10 +57,13 @@
         <h3>Sign up </h3>
         <ul>
             <li>
-                <input type="text" name="screenName" placeholder="Full Name"/>
+                <input type="text" name="screenName" placeholder="Full Name" <?php if (isset($_POST['screenName'])) { ?> value="<?php echo $screenName ?> " <?php } ?> >
+
+
+
             </li>
             <li>
-                <input type="email" name="email" placeholder="Email"/>
+                <input type="text" name="email" placeholder="Email" <?php if (isset($_POST['email'])) { ?> value="<?php echo $email ?> " <?php } ?> />
             </li>
             <li>
                 <input type="password" name="password" placeholder="Password"/>
@@ -18,10 +72,10 @@
                 <input type="submit" name="signup" Value="Signup for Twitter">
             </li>
         </ul>
-        <!--
+       <?php if (isset($error)) { ?>
          <li class="error-li">
-          <div class="span-fp-error"></div>
+          <div class="span-fp-error"><?php echo $error?></div>
          </li>
-        -->
+        <?php } ?>
     </div>
 </form>
